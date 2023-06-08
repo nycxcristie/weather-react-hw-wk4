4f7b54b9c55066bde5943c42e29673723a2e40c4
nycxcristie <104367483+nycxcristie@users.noreply.github.com>
2023-06-08 12:37:39 -0400

    Updated GitHub link to my project in the footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,9 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div className="Footer">
-      <a href="https://github.com/nycxcristie/weatherapp">Open-source code</a>
+      <a href="https://github.com/nycxcristie/weather-react-hw-wk4">
+        Open-source code
+      </a>
       <span> by: Cristina Isaacson</span>
     </div>
   );
